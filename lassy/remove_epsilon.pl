87f88284dbadc69ea89607f8e7e921081fba04e4
Richard Moot <Richard.Moot@labri.fr>
2016-06-14 14:55:42 +0200

    Removed solution

--- lassy/remove_epsilon.pl
+++ lassy/remove_epsilon.pl
@@ -1,17 +1,4 @@
 :- module(remove_epsilon, [remove_epsilon/2]).
 
-remove_epsilon(leaf(A,B,C,D,E,F,G,H), leaf(A,B,C,D,E,F,G,H)).
-remove_epsilon(node(A,B,C,D,E,Ds0), node(A,B,C,D,E,Ds)) :-
-	remove_epsilon1(Ds0, Ds).
+remove_epsilon(Tree, Tree).
 
-remove_epsilon1([], []).
-remove_epsilon1([D0|Ds0], Ds) :-
-    (
-        D0 = epsilon(_,_,_,_,_)
-    ->
-        remove_epsilon1(Ds0, Ds)
-    ;
-        Ds = [D|Ds1],
-        remove_epsilon(D0, D),
-        remove_epsilon1(Ds0, Ds1)
-    ).
