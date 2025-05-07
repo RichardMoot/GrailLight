4f4bd960b4ae5689f5e0fada32232c33b77e8d1b
Richard Moot <richard.moot@lirmm.fr>
2025-05-07 17:40:58 +0200

    Update Supertag.tcl

--- Supertag.tcl
+++ Supertag.tcl
@@ -8,6 +8,8 @@ package require Tk
 package require Mk4tcl
 package require tokenize
 
+#font actual TkFixedFont -family Monaco -size 11 -weight normal -slant roman -underline 0 -overstrike 0
+
 set sent_values {}
 
 set skip 90
